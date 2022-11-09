e7a8ca525e805785ab2ecfa26f146d3c5b5e020b
rodabreu56 <rodrigo.abreu@capeanalytics.com>
2022-11-09 14:16:36 -0600

    Update top_kek_m8.py

--- top_kek_m8.py
+++ top_kek_m8.py
@@ -163,7 +163,7 @@ if uploaded_file is not None:
         #zipcode = address.get('postcode')
         #st.image(filtered_df2[i],caption=f"Imagery Date: {filtered_df.iloc[i]['cape_roof_condition_rating_date']}", width=size)
         img = st.image(filtered_df2[i],width=size)
-        st.write(img)
+        #st.image(img)
         #count += 1
         #st.write(i)
         #st.button('Download',on_click=download_image, args=(filtered_df2[i],str(i)))
